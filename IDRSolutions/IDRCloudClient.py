bb4cef2a3e70dc4eafcf410808756b1f8746b0de
Leon Atherton <db9.leon@gmail.com>
2022-05-16 11:09:40 +0100

    Copyright 2021 → 2022

--- IDRSolutions/IDRCloudClient.py
+++ IDRSolutions/IDRCloudClient.py
@@ -1,5 +1,5 @@
 """
-Copyright 2021 IDRsolutions
+Copyright 2022 IDRsolutions
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
