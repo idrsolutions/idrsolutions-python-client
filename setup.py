aa7213426656079ba48246d91c6b72f06a7e5f6e
Kieran <kieran@idrsolutions.com>
2024-12-16 16:11:06 +0000

    Update version number

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def get_readme():
 
 setup(
     name='IDRCloudClient',
-    version='5.0.0',
+    version='5.0.1',
     description="Python API for IDRSolutions' Microservice Examples",
     long_description=get_readme(),
     long_description_content_type='text/markdown',
